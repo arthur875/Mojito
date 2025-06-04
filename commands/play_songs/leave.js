3b6572e5500c5c9fed886019da395aed2649ecc1
arthur875 <OpenMePls-875@proton.me>
2025-06-04 23:21:22 +0200

    Fix leave command to correctly call distube.voices.leave()

--- commands/play_songs/leave.js
+++ commands/play_songs/leave.js
@@ -11,7 +11,7 @@ module.exports = {
             await interaction.deferReply()
 
             try {
-                await distube.leave()
+                await distube.voices.leave()
                 await interaction.editReply('👋 Left the voice channel!')
             } catch (error) {
                 console.error(`there was an error during the execution of this command: ${error}`)
