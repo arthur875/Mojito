aac4efbfdc791172897d8a05fe12c37b746a7db7
arthur875 <OpenMePls-875@proton.me>
2025-06-07 13:50:19 +0200

    Add queue command to return the current song queue

---
+++ commands/play_songs/queue.js
@@ -0,0 +1,21 @@
+const { SlashCommandBuilder } = require('discord.js')
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('queue')
+        .setDescription('returns the current queue'),
+        async execute(interaction){
+            const { client } = interaction
+            const distube = client.distube
+
+            queue = distube.getQueue(interaction)
+            if (!queue) return interaction.reply('Nothing is playing!')
+
+            interaction.reply(
+                `🎶 Queue:\n${queue.songs
+                    .map((song, i) => `${i === 0 ? '▶️' : `${i}.`} ${song.name} - \`${song.formattedDuration}\``)
+                    .join('\n')}`
+                )    
+        }
+                
+}
