cda0341c75573bd60db42ae7e2284d8e0548cb24
arthur875 <OpenMePls-875@proton.me>
2025-06-09 19:58:40 +0200

    Refactor leave command logic to improve voice channel checks and streamline response handling

--- commands/play_songs/leave.js
+++ commands/play_songs/leave.js
@@ -11,10 +11,12 @@ module.exports = {
             await interaction.deferReply()
 
             try {
-                botVoiceChannel = interaction.guild.members.me.voice.channel;
-                if (!botVoiceChannel) {
+                const botVoiceChannel = interaction.guild.members.me.voice.channel;
+                if (botVoiceChannel) {
                     await distube.voices.leave(interaction)
                     await interaction.editReply('👋 Left the voice channel!')
+                } else {
+                    await interaction.editReply('❌ I am not in a voice channel!')
                 }
             } catch (error) {
                 console.error(`there was an error during the execution of this command: ${error}`)
